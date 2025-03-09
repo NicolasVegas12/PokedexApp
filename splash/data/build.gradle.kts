541f5ecf0f992c53679f9aec9bb15e009b4e648e
Nicolas vegas <nicolas_12_acuario@hotmail.com>
2025-03-09 04:07:59 -0500

    splash modules

--- splash/data/build.gradle.kts
+++ splash/data/build.gradle.kts
@@ -1,34 +1,30 @@
 plugins {
     id(libs.plugins.android.library.get().pluginId)
     id(libs.plugins.kotlin.android.get().pluginId)
-    id(libs.plugins.kotlin.compose.get().pluginId)
     kotlin(libs.plugins.jetbrains.kotlin.serialization.get().pluginId)
     id(libs.plugins.dagger.hilt.get().pluginId)
-    id("org.sonarqube")
     id(libs.plugins.androidx.ksp.get().pluginId)
+    id("org.sonarqube")
+    id("kotlin-parcelize")
     `android-config`
 }
 
 android {
-    namespace = "com.nvegas.splash.presentation"
-    buildFeatures.compose = true
-
+    namespace = "com.nvegas.data"
+    hilt {
+        enableAggregatingTask = false
+    }
 }
 
 dependencies {
-
     implementation(project(":common"))
     implementation(project(":splash:domain"))
-
-    implementation(libs.bundles.android.compose.core)
-    implementation(libs.bundles.android.compose.extensions)
 
     implementation(libs.bundles.dagger.hilt.compose)
 
     ksp(libs.bundles.dagger.hilt.compiler)
 
     implementation(libs.bundles.serialization)
-
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
